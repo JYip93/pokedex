409cf8d7fddaa22f4852053ca2e93e99c2a9e02c
JYip93 <jyippy2012@gmail.com>
2021-01-26 14:30:39 +0000

    console logging data returned from api

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-console.log("hello");
+const fetchPokemon = () => {
+  //   promises = [];
+  //   for (let i = 0; i < 150; i++) {
+  const url = `https://pokeapi.co/api/v2/pokemon/1`;
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data);
+    });
+};
+// };
+
+fetchPokemon();
