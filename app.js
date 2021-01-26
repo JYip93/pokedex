7a1443fd087e60a7db1e33d469c3f67147b10df1
JYip93 <jyippy2012@gmail.com>
2021-01-26 15:27:25 +0000

    Refactor to use Promise.all so all data is fetched at once

--- app.js
+++ app.js
@@ -1,24 +1,35 @@
 const fetchPokemon = () => {
-  //   promises = [];
+  promises = [];
   for (let i = 1; i <= 150; i++) {
     const url = `https://pokeapi.co/api/v2/pokemon/${i}`;
-    fetch(url)
-      .then((res) => res.json())
-      .then((data) => {
-        console.log(data);
-        const pokemon = {
-          id: data.id,
-          name: data.name,
-          image: data.sprites["front_default"],
-          type: data.types
-            .map((type) => {
-              return type.type.name;
-            })
-            .join(", "),
-        };
-        console.log(pokemon);
-      });
+    promises.push(fetch(url).then((res) => res.json()));
+    // console.log(promises);
   }
+
+  Promise.all(promises).then((results) => {
+    // console.log(results);
+    const pokemon = results.map((data) => ({
+      id: data.id,
+      name: data.name,
+      image: data.sprites["front_default"],
+      type: data.types.map((type) => type.type.name).join(", "),
+    }));
+    console.log(pokemon);
+  });
 };
+//   .then((data) => {
+//     console.log(data);
+//     const pokemon = {
+//       id: data.id,
+//       name: data.name,
+//       image: data.sprites["front_default"],
+//       type: data.types
+//         .map((type) => {
+//           return type.type.name;
+//         })
+//         .join(", "),
+//     };
+//     console.log(pokemon);
+//   });
 
 fetchPokemon();
