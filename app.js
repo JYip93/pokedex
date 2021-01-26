56882df7cd90192a2dab30a3b9ae9431d4050b87
JYip93 <jyippy2012@gmail.com>
2021-01-26 14:45:11 +0000

    store api data in JS object

--- app.js
+++ app.js
@@ -1,13 +1,24 @@
 const fetchPokemon = () => {
   //   promises = [];
-  //   for (let i = 0; i < 150; i++) {
-  const url = `https://pokeapi.co/api/v2/pokemon/1`;
-  fetch(url)
-    .then((res) => res.json())
-    .then((data) => {
-      console.log(data);
-    });
+  for (let i = 1; i <= 150; i++) {
+    const url = `https://pokeapi.co/api/v2/pokemon/${i}`;
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        const pokemon = {
+          id: data.id,
+          name: data.name,
+          image: data.sprites["front_default"],
+          type: data.types
+            .map((type) => {
+              return type.type.name;
+            })
+            .join(", "),
+        };
+        console.log(pokemon);
+      });
+  }
 };
-// };
 
 fetchPokemon();
